dbe4dab0514f0d256cf2318c843f47d38dec9cbd
CoolCream <mahasankarshant@gmail.com>
2026-08-20 22:14:11 +0530

    added the history command and functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <windows.h>
 
 #include "parser.h"
@@ -45,8 +46,35 @@ void printWorkingDirectory()
     }
 }
 
+void changeDirectory(const string &path)
+{
+    if (SetCurrentDirectoryA(path.c_str()))
+    {
+        coloredMessage(Color::Green, "Changed directory!");
+    }
+    else
+    {
+        coloredMessage(Color::Red, "Directory not found.");
+    }
+}
+
+void printHelp()
+{
+    coloredMessage(Color::Cyan, "CPSH commands:");
+    cout << "   > help - to show this message\n";
+    cout << "   > home - to open shell home\n";
+    cout << "   > clear - to clear the terminal\n";
+    cout << "   > echo - to print something in the terminal\n";
+    cout << "   > history - to print previously used commands\n";
+    cout << "   > pwd - to print the current working directory\n";
+    cout << "   > cd - to change the working directory in the terminal\n";
+    cout << "   > exit - to exit cpsh\n";
+    cout << "\n";
+}
+
 void printBanner()
 {
+    system("cls");
     coloredMessage(Color::Cyan, R"(
             ╔█████ ╔██████ ╔█████ ╔██ ╔██
             ║██    ║██  ██ ║██    ║██ ║██
@@ -56,6 +84,7 @@ void printBanner()
             ╚════╝ ╚═╝     ╚════╝ ╚═╝ ╚═╝
                   C++ Command Line
     )");
+    printHelp();
 }
 
 int main()
@@ -67,6 +96,7 @@ int main()
     printBanner();
 
     string input;
+    vector<string> commandHistory;
 
     while (true)
     {
@@ -76,6 +106,11 @@ int main()
         getline(cin, input);
         setColor(Color::White);
 
+        if (!input.empty())
+        {
+            commandHistory.push_back(input);
+        }
+
         ParsedCommand parsed = parseCommand(input);
 
         if (parsed.command == "exit")
@@ -83,14 +118,15 @@ int main()
             coloredMessage(Color::Green, "Exiting cpsh...");
             break;
         }
+        if (parsed.command == "home")
+        {
+            printBanner();
+            continue;
+        }
 
         if (parsed.command == "help")
         {
-            coloredMessage(Color::Cyan, "All available commands:");
-            cout << "   > help - to show this message\n";
-            cout << "   > clear - to clear the terminal\n";
-            cout << "   > echo - to print something in the terminal\n";
-            cout << "   > exit - to exit cpsh\n";
+            printHelp();
             continue;
         }
 
@@ -115,6 +151,29 @@ int main()
         if (parsed.command == "pwd")
         {
             printWorkingDirectory();
+            continue;
+        }
+
+        if (parsed.command == "cd")
+        {
+            if (parsed.arguments.empty())
+            {
+                coloredMessage(Color::Yellow, "cd command cannot be empty!, enter a directory path, cd <message>");
+                continue;
+            }
+            changeDirectory(parsed.arguments);
+            continue;
+        }
+        if (parsed.command == "history")
+        {
+            if (commandHistory.empty())
+            {
+                coloredMessage(Color::Yellow, "No commands in history!");
+            }
+            for (size_t i = 0; i < commandHistory.size(); i++)
+            {
+                cout << "  " << i + 1 << ".  " << commandHistory[i] << "\n";
+            }
             continue;
         }
 
