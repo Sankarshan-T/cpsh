dce168295fb1aaa60a906b9709b786f77e4c67ea
CoolCream <mahasankarshant@gmail.com>
2026-08-20 12:41:27 +0530

    added colors to the shell! It looks much cooler now

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 using namespace std;
+
+enum class Color
+{
+    White = 7,
+    Green = 10,
+    Yellow = 14,
+    Red = 12,
+    Cyan = 11,
+    Gray = 8
+};
+
+void setColor(Color color)
+{
+    SetConsoleTextAttribute(
+        GetStdHandle(STD_OUTPUT_HANDLE),
+        static_cast<int>(color));
+}
 
 int main()
 {
@@ -9,19 +27,27 @@ int main()
 
     while (true)
     {
+        setColor(Color::White);
         cout << "cpsh$ ";
+        setColor(Color::Yellow);
         getline(cin, command);
+        setColor(Color::White);
 
         if (command == "exit")
         {
+            setColor(Color::Green);
             cout << "Extiting....\n"
                  << "Exited!";
+
+            setColor(Color::White);
             break;
         }
 
         if (command == "help")
         {
+            setColor(Color::Cyan);
             cout << "All available commands:\n";
+            setColor(Color::White);
             cout << "   > help - to show this message\n";
             cout << "   > clear - to clear the teminal\n";
             cout << "   > exit - to exit cpsh\n";
@@ -30,11 +56,16 @@ int main()
 
         if (command == "clear")
         {
+            setColor(Color::Green);
+            cout << "Extiting....";
+            setColor(Color::White);
             system("cls");
             continue;
         }
 
+        setColor(Color::Red);
         cout << "Unknown Command: " << command << '\n';
+        setColor(Color::White);
     }
 
     return 0;
