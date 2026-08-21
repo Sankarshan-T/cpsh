eeebe9af95a0b28059034aa6035caa5e0ae37a3c
CoolCream <mahasankarshant@gmail.com>
2026-08-21 07:53:49 +0530

    added comments to the commands to remember each function

--- commands.h
+++ commands.h
@@ -15,18 +15,28 @@ enum class Color
     Gray = 8
 };
 
+// Sets the color to the colored message
 void setColor(Color color);
 
+// A simple helper i made to get texts in different colors using the above void.
 void coloredMessage(Color color, const std::string &message);
 
+// void for the pwd command
 void printWorkingDirectory();
 
+// void for the cd command (obv)
 void changeDirectory(const std::string &path);
 
+// void for the help command (made a separate command cuz i wanted to ruse ths in many places)
 void printHelp();
 
+// silly thing for the home screen like the cpsh logo i made using ascii art pff
 void printBanner();
 
+// void for the ls command thingi
+void listDirectory();
+
+// the goat to run all the above commands!!!!
 void executeCommands(
     const string &command,
     const string &argumens,
